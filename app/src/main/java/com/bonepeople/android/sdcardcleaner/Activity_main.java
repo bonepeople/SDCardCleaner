ee7c4b7a3c5dcb6acc5bf5d3436e47598f005b6f
bonepeople <bonepeople@sina.com>
2017-05-23 19:14:35 +0800

    创建工程
    设计程序的主界面及初步的功能规划

---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/Activity_main.java
@@ -0,0 +1,39 @@
+package com.bonepeople.android.sdcardcleaner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
+
+public class Activity_main extends AppCompatActivity implements View.OnClickListener {
+    private CardView _card_scan, _card_white, _card_black;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        _card_scan = (CardView) findViewById(R.id.cardview_scan);
+        _card_white = (CardView) findViewById(R.id.cardview_white);
+        _card_black = (CardView) findViewById(R.id.cardview_black);
+
+        _card_scan.setOnClickListener(this);
+        _card_white.setOnClickListener(this);
+        _card_black.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cardview_scan:
+                Toast.makeText(Activity_main.this, "scan", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cardview_white:
+
+                break;
+            case R.id.cardview_black:
+
+                break;
+        }
+    }
+}
