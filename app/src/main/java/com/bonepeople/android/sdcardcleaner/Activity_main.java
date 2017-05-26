10693b7e926e8064c808d40affd77f73d550970f
bonepeople <bonepeople@sina.com>
2017-05-26 18:01:28 +0800

    修改主界面架构

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/Activity_main.java
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/Activity_main.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class Activity_main extends AppCompatActivity implements View.OnClickListener {
-    private CardView _card_scan, _card_white, _card_black;
+    private CardView _card_scan, _card_white, _card_black, _card_set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +16,12 @@ public class Activity_main extends AppCompatActivity implements View.OnClickList
         _card_scan = (CardView) findViewById(R.id.cardview_scan);
         _card_white = (CardView) findViewById(R.id.cardview_white);
         _card_black = (CardView) findViewById(R.id.cardview_black);
+        _card_set = (CardView) findViewById(R.id.cardview_set);
 
         _card_scan.setOnClickListener(this);
         _card_white.setOnClickListener(this);
         _card_black.setOnClickListener(this);
+        _card_set.setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +34,9 @@ public class Activity_main extends AppCompatActivity implements View.OnClickList
 
                 break;
             case R.id.cardview_black:
+
+                break;
+            case R.id.cardview_set:
 
                 break;
         }
