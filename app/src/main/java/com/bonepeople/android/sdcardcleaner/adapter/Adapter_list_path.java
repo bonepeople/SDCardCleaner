8c7b438be6061e9b92df48732038d551a3705e05
bonepeople <bonepeople@sina.com>
2017-12-09 17:34:38 +0800

    在白名单和黑名单页面中添加空列表展示

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/adapter/Adapter_list_path.java
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/adapter/Adapter_list_path.java
@@ -1,6 +1,7 @@
 package com.bonepeople.android.sdcardcleaner.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
  * Created by bonepeople on 2017/12/5.
  */
 
-public class Adapter_list_path extends RecyclerView.Adapter<Adapter_list_path.ViewHolder> {
+public class Adapter_list_path extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public static final String ACTION_CLICK_ITEM = "click_item";
     private ArrayList<String> _data;
     private View.OnClickListener _listener_click;
@@ -26,31 +27,64 @@ public class Adapter_list_path extends RecyclerView.Adapter<Adapter_list_path.Vi
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        TextView _textView = new TextView(parent.getContext());
-        _textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        _textView.setPadding(20, 20, 20, 20);
-        return new ViewHolder(_textView);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == 1) {
+            TextView _textView = new TextView(parent.getContext());
+            _textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            _textView.setGravity(Gravity.CENTER);
+            return new ViewHolder_empty(_textView);
+        } else {
+            TextView _textView = new TextView(parent.getContext());
+            _textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            _textView.setPadding(20, 20, 20, 20);
+            return new ViewHolder_data(_textView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder._text_name.setText(_data.get(position));
-        holder._text_name.setTag(new String[]{ACTION_CLICK_ITEM, String.valueOf(position)});
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolder_data) {
+            ViewHolder_data _viewHolder = (ViewHolder_data) holder;
+            _viewHolder._text_name.setText(_data.get(position));
+            _viewHolder._text_name.setTag(new String[]{ACTION_CLICK_ITEM, String.valueOf(position)});
+        } else {
+            ViewHolder_empty _viewHolder = (ViewHolder_empty) holder;
+            _viewHolder._text_title.setText("空空如也");
+        }
     }
 
     @Override
     public int getItemCount() {
-        return _data.size();
+        if (_data.size() == 0)
+            return 1;
+        else
+            return _data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        if (_data.size() == 0)
+            return 1;
+        else
+            return super.getItemViewType(position);
+    }
+
+    public class ViewHolder_data extends RecyclerView.ViewHolder {
         public TextView _text_name;
 
-        public ViewHolder(TextView itemView) {
+        public ViewHolder_data(TextView itemView) {
             super(itemView);
             _text_name = itemView;
             _text_name.setOnClickListener(_listener_click);
+        }
+    }
+
+    public class ViewHolder_empty extends RecyclerView.ViewHolder {
+        public TextView _text_title;
+
+        public ViewHolder_empty(TextView itemView) {
+            super(itemView);
+            _text_title = itemView;
         }
     }
 }
