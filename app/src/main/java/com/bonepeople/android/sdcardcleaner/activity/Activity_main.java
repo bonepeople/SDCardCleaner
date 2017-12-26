ca5e66236a1caa12956b05e84bfd1b41971f59b4
bonepeople <bonepeople@sina.com>
2017-12-26 23:00:11 +0800

    调整主界面的布局
    添加ScrollView，解决小屏幕按钮显示不全的问题
    添加载入动画

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/activity/Activity_main.java
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/activity/Activity_main.java
@@ -1,9 +1,11 @@
 package com.bonepeople.android.sdcardcleaner.activity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 
 import com.bonepeople.android.sdcardcleaner.Global;
 import com.bonepeople.android.sdcardcleaner.R;
@@ -11,6 +13,7 @@ import com.bonepeople.android.sdcardcleaner.basic.Basic_appCompatActivity;
 
 public class Activity_main extends Basic_appCompatActivity implements View.OnClickListener {
     private CardView _card_scan, _card_white, _card_black, _card_set;
+    private boolean _shown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,36 @@ public class Activity_main extends Basic_appCompatActivity implements View.OnCli
         _card_set.setOnClickListener(this);
 
         Global.init(getApplicationContext());
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (!_shown)
+            startAnimation();
+    }
+
+    private void startAnimation() {
+        Point _size = new Point();
+        getWindowManager().getDefaultDisplay().getRealSize(_size);
+        int _height = _size.y;
+
+        TranslateAnimation _anim_scan = new TranslateAnimation(0, 0, _height, 0);
+        _anim_scan.setDuration(500);
+        _anim_scan.setStartOffset(0);
+        _card_scan.startAnimation(_anim_scan);
+        TranslateAnimation _anim_white = new TranslateAnimation(0, 0, _height, 0);
+        _anim_white.setDuration(500);
+        _anim_white.setStartOffset(100);
+        _card_white.startAnimation(_anim_white);
+        TranslateAnimation _anim_black = new TranslateAnimation(0, 0, _height, 0);
+        _anim_black.setDuration(500);
+        _anim_black.setStartOffset(200);
+        _card_black.startAnimation(_anim_black);
+        TranslateAnimation _anim_set = new TranslateAnimation(0, 0, _height, 0);
+        _anim_set.setDuration(500);
+        _anim_set.setStartOffset(300);
+        _card_set.startAnimation(_anim_set);
+        _shown = true;
     }
 
     @Override
