4ab7fef2ac25d4c47b3fa085f200164577a9a206
bonepeople <bonepeople@sina.com>
2022-03-17 23:50:02 +0800

    添加设置页面

---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/SettingFragment.kt
@@ -0,0 +1,15 @@
+package com.bonepeople.android.sdcardcleaner.fragment
+
+import android.os.Bundle
+import com.bonepeople.android.base.ViewBindingFragment
+import com.bonepeople.android.sdcardcleaner.databinding.FragmentSettingBinding
+
+class SettingFragment : ViewBindingFragment<FragmentSettingBinding>() {
+    override fun initView() {
+
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+    }
+}
