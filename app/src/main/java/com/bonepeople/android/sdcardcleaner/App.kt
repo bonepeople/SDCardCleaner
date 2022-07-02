069ca5eca056351654d2c0ec69d613fbdc959f45
bonepeople <bonepeople@sina.com>
2022-07-02 22:46:56 +0800

    调整配置文件并更新依赖库版本

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/App.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/App.kt
@@ -1,13 +1,16 @@
 package com.bonepeople.android.sdcardcleaner
 
 import android.app.Application
-import com.bonepeople.android.base.exception.ExceptionHandler
-import com.bonepeople.android.widget.util.AppLog
 
 class App : Application() {
-    init {
-        AppLog.enable = true
-        AppLog.tag = "SDCardCleanerTag"
-        ExceptionHandler.setCrashAction { _, _ -> }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+        const val BUILD_TIME = BuildConfig.buildTime
     }
 }
--- app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/HomeFragment.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/HomeFragment.kt
@@ -1,9 +1,7 @@
 package com.bonepeople.android.sdcardcleaner.fragment
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 import com.bonepeople.android.base.ViewBindingFragment
 import com.bonepeople.android.sdcardcleaner.databinding.FragmentHomeBinding
 import com.bonepeople.android.widget.util.AppPermission
@@ -16,19 +14,14 @@ class HomeFragment : ViewBindingFragment<FragmentHomeBinding>() {
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            startScan()
-        } else {
-            AppPermission.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .onResult { allGranted, _ ->
-                    if (allGranted) {
-                        startScan()
-                    } else {
-                        AppToast.show("需要存储空间的权限才能扫描文件")
-                    }
+        AppPermission.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .onResult { allGranted, _ ->
+                if (allGranted) {
+                    startScan()
+                } else {
+                    AppToast.show("需要存储空间的权限才能扫描文件")
                 }
-                .launch()
-        }
+            }
     }
 
 
