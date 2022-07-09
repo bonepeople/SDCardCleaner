e5f5b139bd8a85e373bf0e1282f5135f7900da6b
bonepeople <bonepeople@sina.com>
2022-07-09 23:39:19 +0800

    在首页添加设置页面的入口

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/HomeFragment.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/HomeFragment.kt
@@ -2,15 +2,24 @@ package com.bonepeople.android.sdcardcleaner.fragment
 
 import android.Manifest
 import android.os.Bundle
+import android.view.View
 import com.bonepeople.android.base.ViewBindingFragment
+import com.bonepeople.android.base.activity.StandardActivity
+import com.bonepeople.android.base.databinding.ViewTitleBinding
+import com.bonepeople.android.sdcardcleaner.R
 import com.bonepeople.android.sdcardcleaner.databinding.FragmentHomeBinding
 import com.bonepeople.android.widget.util.AppPermission
 import com.bonepeople.android.widget.util.AppToast
+import com.bonepeople.android.widget.util.singleClick
 
 class HomeFragment : ViewBindingFragment<FragmentHomeBinding>() {
 
     override fun initView() {
-
+        ViewTitleBinding.bind(views.titleView).run {
+            imageViewTitleAction.setImageResource(R.drawable.icon_set)
+            imageViewTitleAction.visibility = View.VISIBLE
+            imageViewTitleAction.singleClick { openSetting() }
+        }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -24,6 +33,9 @@ class HomeFragment : ViewBindingFragment<FragmentHomeBinding>() {
             }
     }
 
+    private fun openSetting() {
+        StandardActivity.open(activity, SettingFragment())
+    }
 
     private fun startScan() {
         views.storageSummary.refresh()
