dc737f1b2f89bdbfb801d6daa3b65a76aec014bb
bonepeople <bonepeople@sina.com>
2022-07-12 13:34:22 +0800

    重写关于页面，调整设置界面

---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/AboutFragment.kt
@@ -0,0 +1,19 @@
+package com.bonepeople.android.sdcardcleaner.fragment
+
+import android.os.Bundle
+import com.bonepeople.android.base.ViewBindingFragment
+import com.bonepeople.android.base.databinding.ViewTitleBinding
+import com.bonepeople.android.sdcardcleaner.R
+import com.bonepeople.android.sdcardcleaner.databinding.FragmentAboutBinding
+
+class AboutFragment : ViewBindingFragment<FragmentAboutBinding>() {
+    override fun initView() {
+        ViewTitleBinding.bind(views.titleView).run {
+            textViewTitleName.setText(R.string.caption_text_about)
+        }
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+    }
+}
--- app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/SettingFragment.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/SettingFragment.kt
@@ -3,13 +3,27 @@ package com.bonepeople.android.sdcardcleaner.fragment
 import android.os.Bundle
 import com.bonepeople.android.base.ViewBindingFragment
 import com.bonepeople.android.base.activity.StandardActivity
+import com.bonepeople.android.base.databinding.ViewTitleBinding
+import com.bonepeople.android.sdcardcleaner.App
+import com.bonepeople.android.sdcardcleaner.R
 import com.bonepeople.android.sdcardcleaner.databinding.FragmentSettingBinding
+import com.bonepeople.android.widget.ApplicationHolder
+import com.bonepeople.android.widget.util.AppTime
 import com.bonepeople.android.widget.util.singleClick
 
 class SettingFragment : ViewBindingFragment<FragmentSettingBinding>() {
     override fun initView() {
+        ViewTitleBinding.bind(views.titleView).run {
+            textViewTitleName.setText(R.string.caption_text_set)
+        }
         views.textViewWhite.singleClick { StandardActivity.open(activity, CleanPathListFragment.getWhiteList()) }
         views.textViewBlack.singleClick { StandardActivity.open(activity, CleanPathListFragment.getBlackList()) }
+        views.textViewAbout.singleClick { StandardActivity.open(activity, AboutFragment()) }
+        views.textViewVersion.run {
+            val versionName = if (ApplicationHolder.debug) "${ApplicationHolder.getVersionName()} - debug" else ApplicationHolder.getVersionName()
+            val buildTime = AppTime.getDateTimeString(App.BUILD_TIME)
+            text = getString(R.string.app_version, versionName, buildTime)
+        }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
