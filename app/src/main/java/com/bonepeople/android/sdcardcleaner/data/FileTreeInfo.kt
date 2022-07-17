d9c4edaa452419a5788b76a5cbe1c3c1d3c6ddb3
bonepeople <bonepeople@sina.com>
2022-07-17 23:49:55 +0800

    编写新的文件扫描逻辑

---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/data/FileTreeInfo.kt
@@ -0,0 +1,14 @@
+package com.bonepeople.android.sdcardcleaner.data
+
+class FileTreeInfo {
+    var name = ""//文件名
+    var path = ""//文件路径
+    var size = 0L//文件大小
+    var fileCount = 0//文件夹内文件的总数量，包括子文件夹
+    var directory = false//是否是文件夹
+    var rubbish = false//是否需要清理
+    var sorted = 0//当前排序类型，0-未排序，1-按文件名称排序
+    var parent: FileTreeInfo? = null//父目录
+    var largestChild: FileTreeInfo? = null//文件夹内最大的文件
+    var children = ArrayList<FileTreeInfo>()//子文件列表
+}
---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/global/FileTreeManager.kt
@@ -0,0 +1,74 @@
+package com.bonepeople.android.sdcardcleaner.global
+
+import android.os.Environment
+import com.bonepeople.android.sdcardcleaner.data.FileTreeInfo
+import com.bonepeople.android.widget.CoroutinesHolder
+import kotlinx.coroutines.launch
+import java.io.File
+
+object FileTreeManager {
+    const val STATE_READY = 0
+    const val STATE_SCAN_EXECUTING = 1
+    const val STATE_SCAN_STOPPING = 2
+    const val STATE_SCAN_FINISH = 3
+    var rootFile: FileTreeInfo? = null
+    var rubbishCount = 0L
+    var rubbishSize = 0L
+    var state = STATE_READY
+
+    fun startScan() {
+        state = STATE_SCAN_EXECUTING
+        CoroutinesHolder.default.launch {
+            rubbishCount = 0
+            rubbishSize = 0
+            val file = Environment.getExternalStorageDirectory()
+            rootFile = FileTreeInfo()
+            scanFile(null, rootFile!!, file)
+            state = STATE_SCAN_FINISH
+        }
+    }
+
+    fun stopScan() {
+        state = STATE_SCAN_STOPPING
+    }
+
+    private fun scanFile(parentFile: FileTreeInfo?, fileInfo: FileTreeInfo, file: File) {
+        fileInfo.parent = parentFile
+        fileInfo.name = file.name
+        fileInfo.path = file.absolutePath
+        fileInfo.directory = file.isDirectory
+        fileInfo.size = if (fileInfo.directory) 0 else file.length()
+        //垃圾文件的判断
+        when (true) {
+            CleanPathManager.whiteList.contains(fileInfo.path) -> {
+                fileInfo.rubbish = false
+            }
+            CleanPathManager.blackList.contains(fileInfo.path), fileInfo.parent?.rubbish -> {
+                fileInfo.rubbish = true
+                rubbishCount++
+                rubbishSize += fileInfo.size
+            }
+            else -> {
+                fileInfo.rubbish = false
+            }
+        }
+        parentFile?.children?.add(fileInfo)
+        updateParentFile(fileInfo.parent, fileInfo)
+
+        if (fileInfo.directory) {
+            file.listFiles()?.forEach {
+                if (state == STATE_SCAN_EXECUTING) {
+                    scanFile(fileInfo, FileTreeInfo(), it)
+                }
+            }
+        }
+    }
+
+    private fun updateParentFile(parentFile: FileTreeInfo?, fileInfo: FileTreeInfo) {
+        parentFile?.let {
+            it.size += fileInfo.size
+            it.fileCount += 1
+            updateParentFile(it.parent, fileInfo)
+        }
+    }
+}
