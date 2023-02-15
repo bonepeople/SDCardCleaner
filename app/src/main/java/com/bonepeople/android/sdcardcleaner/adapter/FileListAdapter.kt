7255e2cd10d1bf0602296a25bd0978f7bbbf2a6d
bonepeople <bonepeople@sina.com>
2023-02-15 14:47:37 +0800

    补充文件列表的展示逻辑

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/adapter/FileListAdapter.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/adapter/FileListAdapter.kt
@@ -7,39 +7,85 @@ import com.bonepeople.android.base.ViewBindingRecyclerAdapter
 import com.bonepeople.android.sdcardcleaner.R
 import com.bonepeople.android.sdcardcleaner.data.FileTreeInfo
 import com.bonepeople.android.sdcardcleaner.databinding.ItemFileListBinding
+import com.bonepeople.android.sdcardcleaner.fragment.FileListFragment
 import com.bonepeople.android.sdcardcleaner.utils.NumberUtil
 import com.bonepeople.android.widget.util.gone
 import com.bonepeople.android.widget.util.show
 import com.bonepeople.android.widget.util.singleClick
 
-class FileListAdapter(override val list: List<FileTreeInfo>, private val clickAction: (Int) -> Unit) : ViewBindingRecyclerAdapter<ItemFileListBinding, FileTreeInfo>() {
+class FileListAdapter(override val list: List<FileTreeInfo>, private val fragment: FileListFragment) : ViewBindingRecyclerAdapter<ItemFileListBinding, FileTreeInfo>() {
+    var multiSelect = false
+        set(value) {
+            field = value
+            if (value) checkedSet.clear()
+        }
+    val checkedSet = HashSet<Int>()
 
     override fun onBindView(views: ItemFileListBinding, data: FileTreeInfo, position: Int, payloads: MutableList<Any>) {
-        //设置文件大小比例条
-        val percent = NumberUtil.div(data.size.toDouble(), data.parent?.size?.toDouble() ?: 0.0, 2).toFloat()
-        (views.viewPercent.layoutParams as ConstraintLayout.LayoutParams).matchConstraintPercentWidth = percent
-        val color = evaluator.evaluate(percent, 0xFFEAD799.toInt(), 0xFFE96E3E.toInt()) as Int
-        views.viewPercent.setBackgroundColor(color)
-        //设置清理标志
-        if (data.rubbish) {
-            views.imageViewRubbish.show()
-        } else {
-            views.imageViewRubbish.gone()
+        if (payloads.isEmpty()) {
+            //设置文件大小比例条
+            val percent = NumberUtil.div(data.size.toDouble(), data.parent?.size?.toDouble() ?: 0.0, 2).toFloat()
+            (views.viewPercent.layoutParams as ConstraintLayout.LayoutParams).matchConstraintPercentWidth = percent
+            val color = evaluator.evaluate(percent, 0xFFEAD799.toInt(), 0xFFE96E3E.toInt()) as Int
+            views.viewPercent.setBackgroundColor(color)
+            //设置清理标志
+            if (data.rubbish) {
+                views.imageViewRubbish.show()
+            } else {
+                views.imageViewRubbish.gone()
+            }
+            //设置类型图标
+            if (data.directory) {
+                views.imageViewType.setImageResource(R.drawable.icon_directory)
+            } else {
+                views.imageViewType.setImageResource(R.drawable.icon_file)
+            }
+            //设置基本信息
+            views.textViewName.text = data.name
+            views.textViewDescription.text = if (data.directory) {
+                views.root.context.getString(R.string.state_directory_size, Formatter.formatFileSize(views.root.context, data.size), data.fileCount)
+            } else {
+                Formatter.formatFileSize(views.root.context, data.size)
+            }
+            views.root.singleClick(50) { fragment.clickFile(position) }
+            views.root.setOnLongClickListener {
+                if (multiSelect) {
+                    fragment.clickFile(position)
+                } else {
+                    fragment.setMultiSelect(true)
+                    fragment.clickFile(position)
+                }
+                return@setOnLongClickListener true
+            }
         }
-        //设置类型图标
-        if (data.directory) {
-            views.imageViewType.setImageResource(R.drawable.icon_directory)
+        //设置复选框状态
+        if (multiSelect) {
+            views.checkBox.show()
+            views.checkBox.isChecked = checkedSet.contains(position)
         } else {
-            views.imageViewType.setImageResource(R.drawable.icon_file)
+            views.checkBox.gone()
         }
-        //设置基本信息
-        views.textViewName.text = data.name
-        views.textViewDescription.text = if (data.directory) {
-            views.root.context.getString(R.string.state_directory_size, Formatter.formatFileSize(views.root.context, data.size), data.fileCount)
-        } else {
-            Formatter.formatFileSize(views.root.context, data.size)
+    }
+
+    fun updateCheckSet(position: Int): Boolean {
+        if (position == -1) {//全选标记
+            if (checkedSet.size == list.size) {//已全选
+                checkedSet.clear()
+            } else {//未全选
+                checkedSet.addAll(list.indices)
+            }
+        } else {//普通位置序号
+            if (checkedSet.contains(position)) {
+                checkedSet.remove(position)
+            } else {
+                checkedSet.add(position)
+            }
         }
-        views.root.singleClick { clickAction(position) }
+        return checkedSet.size == list.size
+    }
+
+    fun refresh() {
+        notifyItemRangeChanged(0, list.size)
     }
 
     companion object {
--- app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/FileListFragment.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/fragment/FileListFragment.kt
@@ -11,13 +11,22 @@ import com.bonepeople.android.sdcardcleaner.adapter.FileListAdapter
 import com.bonepeople.android.sdcardcleaner.data.FileTreeInfo
 import com.bonepeople.android.sdcardcleaner.databinding.FragmentFileListBinding
 import com.bonepeople.android.sdcardcleaner.global.FileTreeManager
-import com.bonepeople.android.widget.util.hide
-import com.bonepeople.android.widget.util.show
+import com.bonepeople.android.widget.util.*
 
 class FileListFragment(private val file: FileTreeInfo) : ViewBindingFragment<FragmentFileListBinding>() {
+    private val adapter = FileListAdapter(file.children, this)
+
     override fun initView() {
         views.recyclerView.layoutManager = LinearLayoutManager(activity)
         views.recyclerView.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+        views.textViewDelete.singleClick { deleteFiles() }
+        views.textViewClean.singleClick { cleanFiles() }
+        views.textViewHold.singleClick { saveFiles() }
+        views.imageViewClose.singleClick { setMultiSelect(false) }
+        views.checkBoxAll.singleClick(0) {
+            adapter.updateCheckSet(-1)
+            adapter.notifyItemRangeChanged(0, adapter.itemCount, "CheckBox")
+        }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -36,14 +45,55 @@ class FileListFragment(private val file: FileTreeInfo) : ViewBindingFragment<Fra
         } else {
             views.textViewEmpty.hide()
             views.recyclerView.show()
-            views.recyclerView.adapter = FileListAdapter(file.children, this::clickFile)
+            views.recyclerView.adapter = adapter
         }
     }
 
-    private fun clickFile(position: Int) {
-        val child = file.children[position]
-        if (child.directory) {
-            StandardActivity.call(FileListFragment(child)).onResult { views.recyclerView.adapter?.notifyDataSetChanged() }
+    override fun onBackPressed() {
+        if (adapter.multiSelect) {
+            setMultiSelect(false)
+        } else {
+            super.onBackPressed()
         }
+    }
+
+    fun clickFile(position: Int) {
+        if (adapter.multiSelect) {//处于多选状态
+            views.checkBoxAll.isChecked = adapter.updateCheckSet(position)
+            adapter.notifyItemChanged(position, "CheckBox")
+        } else {//处于浏览状态
+            val child = file.children[position]
+            if (child.directory) {
+                StandardActivity.call(FileListFragment(child)).onResult { adapter.refresh() }
+            }
+        }
+    }
+
+    fun setMultiSelect(selecting: Boolean) {
+        adapter.multiSelect = selecting
+        adapter.notifyItemRangeChanged(0, adapter.itemCount, "CheckBox")
+        if (selecting) {
+            views.linearLayoutButtonBar.show()
+        } else {
+            views.linearLayoutButtonBar.gone()
+        }
+    }
+
+    private fun deleteFiles() {
+        setMultiSelect(false)
+        if (adapter.checkedSet.isEmpty()) return
+        adapter.refresh()
+    }
+
+    private fun cleanFiles() {
+        setMultiSelect(false)
+        if (adapter.checkedSet.isEmpty()) return
+        adapter.refresh()
+    }
+
+    private fun saveFiles() {
+        setMultiSelect(false)
+        if (adapter.checkedSet.isEmpty()) return
+        adapter.refresh()
     }
 }
