703f7119e738a9a01b563943a9403e84289d6869
bonepeople <bonepeople@sina.com>
2024-01-20 13:34:21 +0800

    更新Coil的初始化代码，添加Gif扩展库

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/App.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/App.kt
@@ -1,13 +1,16 @@
 package com.bonepeople.android.sdcardcleaner
 
 import android.graphics.drawable.ColorDrawable
-import coil.Coil
+import android.os.Build
 import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.decode.VideoFrameDecoder
 import com.bonepeople.android.base.manager.BaseApp
 import com.bonepeople.android.base.view.TitleView
 
-class App : BaseApp() {
+class App : BaseApp(), ImageLoaderFactory {
     override val appName = "SDCardCleaner"
     override fun onCreate() {
         super.onCreate()
@@ -17,12 +20,22 @@ class App : BaseApp() {
             titleBarBackground = ColorDrawable(getColor(R.color.colorPrimary))
             titleColor = getColor(android.R.color.white)
         }
-        //Coil图片加载库初始化
-        val imageLoader: ImageLoader = ImageLoader.Builder(this)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
             .components {
                 add(VideoFrameDecoder.Factory())
+                // Coil includes two separate decoders to support decoding GIFs.
+                // GifDecoder supports all API levels, but is slower.
+                // ImageDecoderDecoder is powered by Android's ImageDecoder API which is only available on API 28 and above.
+                // ImageDecoderDecoder is faster than GifDecoder and supports decoding animated WebP images and animated HEIF image sequences.
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
             }.build()
-        Coil.setImageLoader(imageLoader)
     }
 
     companion object {
