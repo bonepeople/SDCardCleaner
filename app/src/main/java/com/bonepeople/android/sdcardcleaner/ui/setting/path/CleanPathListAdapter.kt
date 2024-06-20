f25a372a0439c0ee0344aef673657b9af1b0c38e
bonepeople <bonepeople@sina.com>
2024-06-20 22:52:07 +0800

    Refactor CleanPathListAdapter to support DiffUtil and improve item handling
    - Refactored `CleanPathListAdapter` to extend `ViewBindingRefreshAdapter` and leverage `DiffUtil` for efficient list updates.

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/ui/setting/path/CleanPathListAdapter.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/ui/setting/path/CleanPathListAdapter.kt
@@ -1,41 +1,19 @@
 package com.bonepeople.android.sdcardcleaner.ui.setting.path
 
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.bonepeople.android.sdcardcleaner.R
+import com.bonepeople.android.base.viewbinding.ViewBindingRefreshAdapter
+import com.bonepeople.android.sdcardcleaner.data.StringDiffCallback
+import com.bonepeople.android.sdcardcleaner.databinding.ItemPathListBinding
 import com.bonepeople.android.widget.util.AppView.singleClick
 
-class CleanPathListAdapter(private val list: List<String>, private val clickAction: (Int) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == 1) {
-            val textView = TextView(parent.context)
-            textView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-            textView.gravity = Gravity.CENTER
-            textView.setText(R.string.state_emptyView)
-            EmptyHolder(textView)
-        } else {
-            val textView = TextView(parent.context)
-            textView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            textView.setPadding(20, 20, 20, 20)
-            DataHolder(textView)
-        }
+class CleanPathListAdapter : ViewBindingRefreshAdapter<ItemPathListBinding, String>(StringDiffCallback) {
+    private var clickAction: (data: String) -> Unit = {}
+
+    override fun onBindView(views: ItemPathListBinding, data: String, position: Int, payloads: List<Any>) {
+        views.textView.text = data
+        views.textView.singleClick { clickAction(data) }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is DataHolder -> {
-                holder.textView.text = list[position]
-                holder.textView.singleClick { clickAction.invoke(position) }
-            }
-        }
+    fun onCLick(action: (data: String) -> Unit) = apply {
+        clickAction = action
     }
-
-    override fun getItemCount() = if (list.isEmpty()) 1 else list.size
-
-    override fun getItemViewType(position: Int) = if (list.isEmpty()) 1 else super.getItemViewType(position)
-
-    private class DataHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-    private class EmptyHolder(textView: TextView) : RecyclerView.ViewHolder(textView)
 }
