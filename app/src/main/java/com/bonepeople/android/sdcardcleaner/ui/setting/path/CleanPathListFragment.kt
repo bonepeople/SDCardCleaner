4123a1fd328dc5096ba6092e0142cf6518c4a3d8
bonepeople <bonepeople@qq.com>
2025-02-20 11:26:03 +0800

    Migrate CleanPathListFragment to use ViewModel
    - Moved `CleanPathListFragment` and its related adapter to the `path` package for better organization.
    - Refactored `CleanPathListFragment` to implement the ViewModel architecture by adding `CleanPathListViewModel`.
    - Replaced `mode` constants with an enum-like `Mode` object inside `CleanPathListFragment` for better maintainability.

--- app/src/main/java/com/bonepeople/android/sdcardcleaner/ui/setting/path/CleanPathListFragment.kt
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/ui/setting/path/CleanPathListFragment.kt
@@ -1,8 +1,14 @@
-package com.bonepeople.android.sdcardcleaner.ui.setting
+package com.bonepeople.android.sdcardcleaner.ui.setting.path
 
 import android.os.Bundle
 import android.os.Environment
 import androidx.appcompat.app.AlertDialog
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.DEFAULT_ARGS_KEY
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bonepeople.android.base.viewbinding.ViewBindingFragment
@@ -13,9 +19,10 @@ import com.bonepeople.android.sdcardcleaner.global.FileTreeManager
 import com.bonepeople.android.widget.util.AppToast
 
 class CleanPathListFragment : ViewBindingFragment<FragmentCleanPathListBinding>() {
+    private val viewModel: CleanPathListViewModel by viewModels { ViewModelFactory }
     private val listData = arrayListOf<String>()
     private val adapter = CleanPathListAdapter(listData, this::onItemClick)
-    private var mode = WHITE
+    private var mode = Mode.WHITE
     override fun initView() {
         views.recyclerView.layoutManager = LinearLayoutManager(activity)
         views.recyclerView.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
@@ -23,17 +30,17 @@ class CleanPathListFragment : ViewBindingFragment<FragmentCleanPathListBinding>(
 
     @Suppress("deprecation")
     override fun initData(savedInstanceState: Bundle?) {
-        mode = arguments?.getInt("mode", WHITE) ?: WHITE
+        mode = arguments?.getInt("mode", Mode.WHITE) ?: Mode.WHITE
         val basicPath = Environment.getExternalStorageDirectory().path
         when (mode) {
-            WHITE -> {
+            Mode.WHITE -> {
                 views.titleView.title = getString(R.string.caption_text_white)
                 CleanPathManager.whiteList.forEach {
                     listData.add(it.replace(basicPath, getString(R.string.str_path_rootFile)))
                 }
             }
 
-            BLACK -> {
+            Mode.BLACK -> {
                 views.titleView.title = getString(R.string.caption_text_black)
                 CleanPathManager.blackList.forEach {
                     listData.add(it.replace(basicPath, getString(R.string.str_path_rootFile)))
@@ -52,7 +59,7 @@ class CleanPathListFragment : ViewBindingFragment<FragmentCleanPathListBinding>(
             AlertDialog.Builder(requireActivity())
                 .setMessage(getString(R.string.dialog_list_path_remove, listData[index]))
                 .setPositiveButton(android.R.string.ok) { _, _ ->
-                    if (mode == WHITE) {
+                    if (mode == Mode.WHITE) {
                         CleanPathManager.removeWhiteList(index)
                     } else {
                         CleanPathManager.removeBlackList(index)
@@ -66,15 +73,23 @@ class CleanPathListFragment : ViewBindingFragment<FragmentCleanPathListBinding>(
         }
     }
 
-    companion object {
+    object Mode {
+        const val UNKNOWN = 0
         const val WHITE = 1
         const val BLACK = 2
+    }
 
+    companion object {
         fun newInstance(mode: Int): CleanPathListFragment {
-            return CleanPathListFragment().apply {
-                arguments = Bundle().apply {
-                    putInt("mode", mode)
-                }
+            return CleanPathListFragment().apply { arguments = bundleOf("mode" to mode) }
+        }
+
+        private object ViewModelFactory : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                val arguments: Bundle = extras[DEFAULT_ARGS_KEY] ?: Bundle()
+                val mode: Int = arguments.getInt("mode", Mode.UNKNOWN)
+                return CleanPathListViewModel(mode) as T
             }
         }
     }
---
+++ app/src/main/java/com/bonepeople/android/sdcardcleaner/ui/setting/path/CleanPathListViewModel.kt
@@ -0,0 +1,9 @@
+package com.bonepeople.android.sdcardcleaner.ui.setting.path
+
+import androidx.lifecycle.ViewModel
+
+class CleanPathListViewModel(private val mode: Int) : ViewModel() {
+    init {
+
+    }
+}
